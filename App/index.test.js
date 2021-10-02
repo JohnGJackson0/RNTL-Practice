a9f1f50e0a2140cd6f65c22f906f97c12546d628
jgjio <johngjackson0@gmail.com>
2021-10-02 10:29:09 -0400

    run first test

---
+++ App/index.test.js
@@ -0,0 +1,7 @@
+//warning:
+//use actual versions below, each version seems to break other tests
+//yarn add --dev jest@~26.6.3 jest-expo@~41.0.0 jest-fetch-mock@~3.0.3 @testing-library/react-native@~7.2.0
+
+test("it works", () => {
+  expect(true).toBeTruthy();
+});
